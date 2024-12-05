77b1581f5f30a7d39f01c50928da639e7fd87c89
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2024-12-05 11:14:36 +0530

    windows

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-black text-white'>what is your name</h1>
+      <h1 className=' bg-amber-500 text-white'>what is your name
+        <div className='bg-black'>
+          hawhf
+        </div>
+      </h1>
     </>
   )
 }
