67c0e318515fc7342234bf213d7dff1b24dc8d20
arshitrupanii <arshitrupani6@gmail.com>
2025-01-14 10:10:55 +0530

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -12,7 +12,7 @@ const container = () => {
     }
 
     const handlecheckbox = (e) => {
-      
+        settodo(e.target.value)
     }
     
     const handleAdd = () => {
@@ -20,21 +20,19 @@ const container = () => {
         settodo("")
     }
 
-
-
     return (
         <div>
-            <div className="w-[40%] h-[42vw] bg-indigo-100 rounded-xl m-auto mt-5 p-4">
-                <p className="font-bold text-[30px] flex justify-center p-3 ">iTask - Manage your Task at one place</p>
+            <div className="w-[40%] h-[80vh] bg-indigo-100 rounded-xl m-auto mt-5 p-5">
+                <p className="font-bold text-[30px] flex justify-center p-3 ">iTask - Manage your Task</p>
 
-                <label className="font-bold text-[26px] mt-4" htmlFor="add">Add a Todo</label>
+                <label className="font-bold text-[22px] flex justify-center" htmlFor="add">Add a Todo</label>
 
-                <div className="flex items-center">
+                <div className="flex items-center justify-center">
                     <input onChange={handleChange} value={todo} className="h-[30px] w-[75%] mt-3 rounded-3xl px-3" type="text" />
                     <button onClick={handleAdd} className='bg-violet-800 mt-3 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-4 py-2 text-sm font-bold text-white'>Add</button>
                 </div>
 
-                <div className="flex mb-5 gap-2 mt-5">
+                <div className="flex mb-5 gap-2 mt-5 justify-center items-center">
                     <input type="checkbox" name="show_finished" id="" />
                     <label className="" htmlFor="finished">Show finished</label>
                 </div>
@@ -42,19 +40,22 @@ const container = () => {
                 <div className="border m-auto border-gray-400 opacity-35 w-[90%]"></div>
 
                 <div className="mt-3">
-                    <p className="text-[25px] font-bold">Your todos</p>
+                    <p className="text-[25px] font-bold flex justify-center">Your todos</p>
 
                     {/* this all the tasks */}
 
                     {todos.map(item => {
-                         return (<div key={item.id} className="flex gap-4 mt-2 ">
-                            <div className="flex gap-4 mt-2 justify-center">
+                         return (<div key={item.id} className="flex gap-4 mt-2 pl-2">
+
+                            <div className="flex gap-4 mt-2 justify-start items-start">
                                 <input type="checkbox" value={item.iscompleted} onChange={item.iscompleted =true} name="task_finished_not" id="" />
-                                <p className={item.iscompleted ? "line-through" : ""}>{item.todo}</p>
+                                <p className={item.iscompleted ? "line-through items-start" : ""}>{item.todo}</p>
                             </div>
 
-                            <div>
+
+                            <div className="flex items-center justify-center">
                                 <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><img className="w-[20px]" src="https://img.icons8.com/?size=100&id=6697&format=png&color=FFFFFF" alt="" /></button>
+
                                 <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'><img className="w-[20px]" src="https://img.icons8.com/?size=100&id=67884&format=png&color=FFFFFF" alt="" /></button>
 
                             </div>
